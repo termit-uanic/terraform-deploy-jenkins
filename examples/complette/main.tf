680f68952483055aaea9ac98c770193fe625d4c8
Yuriy Poltorak <yuriy.poltorak@gmail.com>
2020-12-02 14:26:24 +0200

    Added example

---
+++ examples/complette/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = var.region
+}
+
+module "deploy_jenkins" {
+  source = "../../"
+
+  region            = "eu-central-1"
+  availability_zone = "c"
+  instance_type     = "t2.micro"
+  key_name          = "jenkins"
+}
